f68616397a5d8baa808f6736501e339cdcee6e07
Andrew-Neely-82 <andrew.neely82@gmail.com>
2023-03-29 18:53:36 -0500

    added mixins / js features / styling / no script

--- js/app.js
+++ js/app.js
@@ -1,30 +1,88 @@
-let sections = document.querySelectorAll("section");
-let navLinks = document.querySelectorAll("header nav a");
+const highlightActiveNavLink = () => {
+  const sections = document.querySelectorAll("section");
+  const navLinks = document.querySelectorAll("header nav a");
 
-window.onscroll = () => {
-  sections.forEach((section) => {
-    let top = window.scrollY;
-    let offset = section.offsetTop - 150;
-    let height = section.offsetHeight;
-    let id = section.getAttribute("id");
+  window.addEventListener("scroll", () => {
+    sections.forEach((section) => {
+      const top = window.scrollY;
+      const offset = section.offsetTop - 150;
+      const height = section.offsetHeight;
+      const id = section.getAttribute("id");
 
-    if (top >= offset && top < offset + height) {
-      navLinks.forEach((links) => {
-        links.classList.remove("active");
-        document.querySelector("header nav a[href*=" + id + "]").classList.add("active");
-      });
-    }
+      if (top >= offset && top < offset + height) {
+        navLinks.forEach((link) => {
+          link.classList.remove("active");
+          document.querySelector(`header nav a[href*=${id}]`).classList.add("active");
+        });
+      }
+    });
   });
-  let header = document.querySelector("header");
-  header.classList.toggle("sticky", window.scrollY > 100);
-  menuIcon.classList.remove("bx-x");
-  navbar.classList.remove("active");
 };
 
-let menuIcon = document.querySelector("#menu-icon");
-let navbar = document.querySelector(".navbar");
+const toggleStickyHeader = () => {
+  const header = document.querySelector("header");
+  const threshold = 100;
 
-menuIcon.onclick = () => {
-  menuIcon.classList.toggle("bx-x");
-  navbar.classList.toggle("active");
+  window.addEventListener("scroll", () => {
+    header.classList.toggle("sticky", window.scrollY > threshold);
+  });
 };
+
+const toggleMobileMenu = () => {
+  const menuIcon = document.querySelector("#menu-icon");
+  const navbar = document.querySelector(".navbar");
+
+  menuIcon.addEventListener("click", () => {
+    menuIcon.classList.toggle("bx-x");
+    navbar.classList.toggle("active");
+  });
+};
+
+const animateContactPlaceholders = () => {
+  const contactSection = document.getElementById("contact");
+  const inputs = document.querySelectorAll("input, textarea");
+  let animated = false;
+
+  const animatePlaceholders = () => {
+    inputs.forEach((input, index) => {
+      const placeholder = input.getAttribute("placeholder");
+      input.setAttribute("placeholder", "");
+
+      setTimeout(() => {
+        typePlaceholder(input, placeholder, 0);
+      }, index * 100);
+    });
+  };
+
+  const typePlaceholder = (input, placeholder, charIndex) => {
+    if (charIndex < placeholder.length) {
+      input.setAttribute("placeholder", input.getAttribute("placeholder") + placeholder[charIndex]);
+      setTimeout(() => {
+        typePlaceholder(input, placeholder, charIndex + 1);
+      }, 50);
+    }
+  };
+
+  const checkVisibility = () => {
+    const rect = contactSection.getBoundingClientRect();
+    const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+    const isVisible = rect.top <= windowHeight && rect.top + rect.height >= 0;
+
+    if (isVisible && !animated) {
+      animatePlaceholders();
+      animated = true;
+    }
+  };
+
+  checkVisibility();
+  document.addEventListener("scroll", checkVisibility);
+};
+
+const init = () => {
+  highlightActiveNavLink();
+  toggleStickyHeader();
+  toggleMobileMenu();
+  animateContactPlaceholders();
+};
+
+init();
