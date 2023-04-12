8fe4af2fe598e6809795cc979d9dd2250b3a365f
Andrew-Neely-82 <andrew.neely82@gmail.com>
2023-04-11 20:40:55 -0500

    adding styling and media based UX

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import './about.scss'
+import "./about.scss";
 
 const About = () => {
   return (
@@ -14,11 +14,13 @@ const About = () => {
         </h2>
         <h3>Web Developer</h3>
         <p>Having grown up in a military family, I had the opportunity to travel extensively across the United States. Upon graduating from Copperas Cove High School, I proudly served in the United States Army as a Signal Systems Support Specialist. Embracing challenges, I chose to become a paratrooper and had the privilege of serving with the renowned 82nd Airborne Division and 173rd Airborne Brigade. After completing my military service, I embarked on a new journey in web development by enrolling in Codeup, a reputable coding bootcamp based in San Antonio, Texas.</p>
-        <p style={{ color: "yellow" }}>Button below for more info coming soon:</p>
-        <p style={{ color: "yellow", marginTop: 0, padding: 0 }}>Click it anyways to see a cool effect!</p>
-        <a href="#about" class="btn">
-          Read More
-        </a>
+        <div className="btn-container">
+          <span style={{ color: "yellow" }}>Button below for more info coming soon:</span>
+          <span style={{ color: "yellow", marginBottom: "2rem", padding: 0 }}>Click it anyways to see a cool effect!</span>
+          <a href="#about" class="btn">
+            Read More
+          </a>
+        </div>
       </div>
     </section>
   );
--- src/sections/skills/Skills.jsx
+++ src/sections/skills/Skills.jsx
@@ -1,92 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 import "./skills.scss";
 
+const skillsData = [
+  {
+    name: "HTML 5",
+    icon: "bx bxl-html5",
+    description: "Experienced in crafting semantic and accessible HTML5 markup for responsive and modern web applications. Familiar with SEO best practices and performance optimization techniques.",
+    link: "#skills",
+  },
+  {
+    name: "CSS/SCSS",
+    icon: "devicon-sass-original",
+    description: "Skilled in using CSS and SCSS/Sass to create visually appealing and responsive designs. Proficient in implementing animations, transitions, and working with CSS frameworks like Bootstrap and Tailwind CSS.",
+    link: "#skills",
+  },
+  {
+    name: "Javascript",
+    icon: "bx bxl-javascript",
+    description: "Strong knowledge of modern JavaScript (ES6+) concepts and practices, including asynchronous programming, DOM manipulation, and working with AJAX and RESTful APIs. Familiar with popular libraries such as jQuery.",
+    link: "https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/What_is_JavaScript",
+  },
+  {
+    name: "React.js",
+    icon: "devicon-react-original",
+    description: "Proficient in building user interfaces with React.js, utilizing functional components and hooks. Experienced in working with state management libraries like Redux, and implementing server-side rendering with Next.js.",
+    link: "https://legacy.reactjs.org/",
+  },
+  {
+    name: "MySQL",
+    icon: "devicon-mysql-plain",
+    description: "Well-versed in designing and managing relational databases using MySQL, with a solid understanding of SQL queries, joins, and transactions. Experienced in optimizing database performance and ensuring data security.",
+    link: "https://dev.mysql.com/doc/refman/8.0/en/what-is-mysql.html",
+  },
+  {
+    name: "Java",
+    icon: "devicon-java-plain",
+    description: "Competent in Java programming, with a focus on Java EE and Spring Boot for creating scalable and robust web applications. Knowledgeable in working with multi-threading, data structures, and various design patterns.",
+    link: "https://www.w3schools.com/java/java_intro.asp",
+  },
+];
+
 const Skills = () => {
+  const [openSkillIndex, setOpenSkillIndex] = useState(-1);
+  const toggleSkill = (index) => {
+    setOpenSkillIndex(openSkillIndex === index ? -1 : index);
+  };
+
   return (
-    <section class="skills" id="skills">
-      <h2 class="heading">
+    <section className="skills" id="skills">
+      <h2 className="heading">
         My <span>&nbsp;Skills</span>
       </h2>
 
-      <div class="skills-container">
-        <div class="skills-box">
-          <div class="skills-box-header">
-            <i class="bx bxl-html5"></i>
-            <h3 style={{ display: "inline", textAlign: "center" }}>HTML 5</h3>
-          </div>
-          <p>Experienced in crafting semantic and accessible HTML5 markup for responsive and modern web applications. Familiar with SEO best practices and performance optimization techniques.</p>
-          <br />
-          <div class="btn-container">
-            <a href="#skills" class="btn">
-              Read More
-            </a>
-          </div>
-        </div>
+      <div className="skills-container">
+        {skillsData.map((skill, index) => {
+          const displayValue = openSkillIndex === index ? "flex" : "none";
 
-        <div class="skills-box">
-          <div class="skills-box-header">
-            <i class="devicon-sass-original"></i>
-            <h3>CSS/SCSS</h3>
-          </div>
-          <p>Skilled in using CSS and SCSS/Sass to create visually appealing and responsive designs. Proficient in implementing animations, transitions, and working with CSS frameworks like Bootstrap and Tailwind CSS.</p>
-          <div class="btn-container">
-            <a href="#skills" class="btn">
-              Read More
-            </a>
-          </div>
-        </div>
-
-        <div class="skills-box">
-          <div class="skills-box-header">
-            <i class="bx bxl-javascript"></i>
-            <h3>Javascript</h3>
-          </div>
-          <p>Strong knowledge of modern JavaScript (ES6+) concepts and practices, including asynchronous programming, DOM manipulation, and working with AJAX and RESTful APIs. Familiar with popular libraries such as jQuery.</p>
-          <div class="btn-container">
-            <a href="https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/What_is_JavaScript" target="_blank" class="btn">
-              Read More
-            </a>
-          </div>
-        </div>
-
-        <div class="skills-box">
-          <div class="skills-box-header">
-            <i class="devicon-react-original"></i>
-            <h3>React.js</h3>
-          </div>
-          <p>Proficient in building user interfaces with React.js, utilizing functional components and hooks. Experienced in working with state management libraries like Redux, and implementing server-side rendering with Next.js.</p>
-          <div class="btn-container">
-            <a href="https://legacy.reactjs.org/" target="_blank" class="btn">
-              Read More
-            </a>
-          </div>
-        </div>
-
-        <div class="skills-box">
-          <div class="skills-box-header">
-            <i class="devicon-mysql-plain"></i>
-            <h3>MySQL</h3>
-          </div>
-          <p>Well-versed in designing and managing relational databases using MySQL, with a solid understanding of SQL queries, joins, and transactions. Experienced in optimizing database performance and ensuring data security.</p>
-          <div class="btn-container">
-            <a href="https://dev.mysql.com/doc/refman/8.0/en/what-is-mysql.html" target="_blank" class="btn">
-              Read More
-            </a>
-          </div>
-        </div>
-
-        <div class="skills-box">
-          <div class="skills-box-header">
-            <i class="devicon-java-plain"></i>
-            <h3>Java</h3>
-          </div>
-          <p>Competent in Java programming, with a focus on Java EE and Spring Boot for creating scalable and robust web applications. Knowledgeable in working with multi-threading, data structures, and various design patterns.</p>
-          <div class="btn-container">
-            <a href="https://www.w3schools.com/java/java_intro.asp" target="_blank" class="btn">
-              Read More
-            </a>
-          </div>
-        </div>
+          return (
+            <div className="skills-box" key={index}>
+              <div className="skills-box-header" onClick={() => toggleSkill(index)}>
+                <i className={skill.icon}></i>
+                <h3>{skill.name}</h3>
+                <i className={`bx ${openSkillIndex === index ? "bx-up-arrow-alt" : "bx-down-arrow-alt"}`}></i>
+              </div>
+              <p className="skill-description" style={{ display: displayValue }}>
+                {skill.description}
+              </p>
+              <div className="btn-container" style={{ display: displayValue }}>
+                <a href={skill.link} target="_blank" className="btn">
+                  Read More
+                </a>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </section>
   );
