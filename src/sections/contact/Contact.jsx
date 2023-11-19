67cf5907e818a64d4bb8a7318ab1a29c5d561b04
Andrew-Neely-82 <andrew.neely82@gmail.com>
2023-11-19 16:18:42 -0600

    form working now

--- src/sections/contact/Contact.jsx
+++ src/sections/contact/Contact.jsx
@@ -1,81 +1,15 @@
 import { SectionHeading } from "../../components/customHTML/export";
-import { useState, useEffect, useRef } from "react";
+import ContactForm from "./ContactForm";
 import "./contact.scss";
 
 const Contact = () => {
-  const useAnimatedPlaceholders = (refs) => {
-    const [animated, setAnimated] = useState(false);
-
-    useEffect(() => {
-      const animatePlaceholders = () => {
-        if (animated) return;
-        setAnimated(true);
-
-        refs.forEach((ref, index) => {
-          const input = ref.current;
-          const placeholder = input.getAttribute("placeholder");
-          input.setAttribute("placeholder", "");
-
-          setTimeout(() => {
-            typePlaceholder(input, placeholder, 0);
-          }, index * 100);
-        });
-      };
-
-      const typePlaceholder = (input, placeholder, charIndex) => {
-        if (charIndex < placeholder.length) {
-          input.setAttribute("placeholder", input.getAttribute("placeholder") + placeholder[charIndex]);
-          setTimeout(() => {
-            typePlaceholder(input, placeholder, charIndex + 1);
-          }, 50);
-        }
-      };
-
-      const checkVisibility = () => {
-        const contactSection = document.getElementById("contact");
-        const rect = contactSection.getBoundingClientRect();
-        const windowHeight = window.innerHeight || document.documentElement.clientHeight;
-        const isVisible = rect.top <= windowHeight && rect.top + rect.height >= 0;
-
-        if (isVisible) {
-          animatePlaceholders();
-        }
-      };
-
-      checkVisibility();
-      document.addEventListener("scroll", checkVisibility);
-
-      return () => {
-        document.removeEventListener("scroll", checkVisibility);
-      };
-    }, [refs, animated]);
-  };
-  const nameRef = useRef();
-  const emailRef = useRef();
-  const numberRef = useRef();
-  const subjectRef = useRef();
-  const messageRef = useRef();
-
-  useAnimatedPlaceholders([nameRef, emailRef, numberRef, subjectRef, messageRef]);
-
   return (
     <section className="contact" id="contact">
       <div className="wrapper">
         <SectionHeading text="Contact" span="Me!" />
-        <h6 className="yellow-text">Updating the form below soon!</h6>
-        <h6 className="yellow-text">Please use Linkedin button above in Home section</h6>
-        <form action={`#`}>
-          <div className="input-box">
-            <input ref={nameRef} type="text" name="name" id="name" placeholder="Full Name" />
-            <input ref={emailRef} type="email" name="email" id="email" placeholder="Email Address" />
-          </div>
-          <div className="input-box">
-            <input ref={numberRef} type="text" name="number" id="number" placeholder="Contact Number" />
-            <input ref={subjectRef} type="text" name="subject" id="subject" placeholder="Email Subject" />
-          </div>
-          <textarea ref={messageRef} name="" id="message" cols="30" rows="10" placeholder="Your Message ..." />
-          <input className="btn" type="submit" value="Send " />
-        </form>
+        <h6 className="yellow-text">Send me a message!</h6>
+        {/* <h6 className="yellow-text">Please use Linkedin button above in Home section</h6> */}
+        <ContactForm />
         <div id="bottom"></div>
       </div>
     </section>
---
+++ src/sections/contact/ContactForm.jsx
@@ -0,0 +1,84 @@
+import React, { useEffect, useRef, useState } from "react";
+import { useForm, ValidationError } from "@formspree/react";
+
+function useAnimatedPlaceholders(refs) {
+  const [animated, setAnimated] = useState(false);
+
+  useEffect(() => {
+    const animatePlaceholders = () => {
+      if (animated) return;
+      setAnimated(true);
+
+      refs.forEach((ref, index) => {
+        // Ensure that ref.current is defined
+        if (!ref.current) return;
+
+        const input = ref.current;
+        const placeholder = input.getAttribute("placeholder");
+        input.setAttribute("placeholder", "");
+
+        setTimeout(() => {
+          typePlaceholder(input, placeholder, 0);
+        }, index * 100);
+      });
+    };
+
+    const typePlaceholder = (input, placeholder, charIndex) => {
+      if (charIndex < placeholder.length) {
+        input.setAttribute("placeholder", input.getAttribute("placeholder") + placeholder[charIndex]);
+        setTimeout(() => {
+          typePlaceholder(input, placeholder, charIndex + 1);
+        }, 50);
+      }
+    };
+
+    const checkVisibility = () => {
+      const contactSection = document.getElementById("contact");
+      if (!contactSection) return;
+
+      const rect = contactSection.getBoundingClientRect();
+      const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+      const isVisible = rect.top <= windowHeight && rect.top + rect.height >= 0;
+
+      if (isVisible) {
+        animatePlaceholders();
+      }
+    };
+
+    checkVisibility();
+    document.addEventListener("scroll", checkVisibility);
+
+    return () => {
+      document.removeEventListener("scroll", checkVisibility);
+    };
+  }, [refs, animated]);
+}
+
+
+function ContactForm() {
+  const [state, handleSubmit] = useForm("xjvqdjpk");
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const subjectRef = useRef();
+  const messageRef = useRef();
+
+  useAnimatedPlaceholders([nameRef, emailRef, subjectRef, messageRef]);
+
+  if (state.succeeded) {
+    return <p>Thank you!</p>;
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input ref={emailRef} type="email" name="email" id="email" placeholder="Your Email Address" />
+      <ValidationError prefix="Email" field="email" errors={state.errors} />
+      <textarea ref={messageRef} name="message" id="message" cols="30" rows="10" placeholder="Your Message ..." />
+      <ValidationError prefix="Message" field="message" errors={state.errors} />
+      <button className="btn" type="submit" disabled={state.submitting}>
+        Submit
+      </button>
+    </form>
+  );
+}
+
+export default ContactForm;
