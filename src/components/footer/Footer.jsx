1acd842d332ec49d11a323a5e8c430cd4e02459c
Andrew-Neely-82 <andrew.neely82@gmail.com>
2024-01-23 14:48:32 -0600

    updated date on footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -27,7 +27,7 @@ const Footer = () => {
         <div className="footer-text">
           <p>
             Last updated on &nbsp;
-            <span style={{ color: "yellow", textShadow: "none" }}>19 November 2023 &nbsp;</span>
+            <span style={{ color: "yellow", textShadow: "none" }}>23 January 2024 &nbsp;</span>
           </p>
         </div>
         <div className="footer-iconTop">
